6e2eab66d84cd722c49a0c4a731ffb29fe025c5a
Charles Weng <hueborg@proton.me>
2024-05-07 19:30:05 -0700

    fix jinja2

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html', title="Welcome", name="World")
 
 app.run(host="127.0.0.1",port=8080,debug=True)
