1c105593c159fa658a635204765a6a3ed6f08457
Charles Weng <hueborg@proton.me>
2024-05-14 10:50:37 -0700

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,14 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Customer (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    password VARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS Account (
+		account_id INT AUTO_INCREMENT PRIMARY KEY,
+		customer_id int,
+		account_name VARCHAR(50) NOT NULL,
+		balance INT,
+		account_type VARCHAR(100) NOT NULL,
+		CONSTRAINT FK_CustomerAccount FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 );
