2af17333680fc76ae1bd407f70c2afaf1c27c777
Matthew Sah <mattsah2001@gmail.com>
2024-05-14 12:31:31 -0700

    auth service

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html', title="Welcome", name="World")
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     username = request.form['username']
+#     # Assuming authentication is successful
+#     session['username'] = username
+#     return 'Logged in successfully!'
+
+# @app.route('/profile')
+# def profile():
+#     if 'username' in session:
+#         return 'Logged in as %s' % session['username']
+#     return 'You are not logged in'
+
+# @app.route('/logout')
+# def logout():
+#     session.pop('username', None)
+#     return 'Logged out successfully'
 
 app.run(host="0.0.0.0",port=8080,debug=True)
--- authservice.py
+++ authservice.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import hashlib
+from flask import session
 
 config = {
     'user': 'root',
@@ -52,3 +53,17 @@ class AuthService:
         sha1_hash.update(password.encode('utf-8'))
         hashed_pass = sha1_hash.hexdigest()
         query = f'SELECT customer_id FROM Customer WHERE username={username} AND password={hashed_pass}'
+        
+        customer_id = None
+        
+        try:
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+            customer_id = cursor.execute(query)
+        finally:
+            cursor.close()
+            conn.close()
+        
+        return customer_id
+
+    
